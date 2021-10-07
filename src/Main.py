2e88a1ed89a3438bdc54471b35da14d04223c571
emanuel.durso <emanuel.durso@keyrus.com>
2021-10-07 15:46:36 -0300

    Initial commit

---
+++ src/Main.py
@@ -0,0 +1,24 @@
+
+from Wallet import Wallet
+from TransactionPool import TransactionPool
+from Block import Block
+
+if __name__ == '__main__':
+
+    sender = 'sender'
+    receiver = 'receiver'
+    amount = 1.0
+    type = 'TRANSFER'
+
+    wallet = Wallet()
+    fraudulentWallet = Wallet()
+    pool = TransactionPool()
+    
+    transaction = wallet.createTransaction(receiver, amount, type)
+
+    if pool.transactionExists(transaction) == False:
+        pool.addTransaction(transaction)
+
+    block = Block(pool.transactions, 'lastHash', 'forger', 1)
+
+    print(block.toJson())
