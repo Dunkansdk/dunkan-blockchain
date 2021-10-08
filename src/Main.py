5e69c64dbe68a3a6c3709e7b01a4a965820f47e4
Dunkansdk <emanueldurso1@gmail.com>
2021-10-07 22:09:18 -0300

    Added AccountModel component to interact with wallets and blocks

--- src/Main.py
+++ src/Main.py
@@ -6,37 +6,17 @@ from Wallet import Wallet
 from TransactionPool import TransactionPool
 from Block import Block
 from Blockchain import Blockchain
+from AccountModel import AccountModel
 
 if __name__ == '__main__':
-
-    sender = 'sender'
-    receiver = 'receiver'
-    amount = 1.0
-    type = 'TRANSFER'
-
-    wallet = Wallet()
-    fraudulentWallet = Wallet()
-    pool = TransactionPool()
     
-    transaction = wallet.createTransaction(receiver, amount, type)
+    wallet = Wallet()
+    accountModel = AccountModel()
 
-    if pool.transactionExists(transaction) == False:
-        pool.addTransaction(transaction)
+    accountModel.updateBalance(wallet.publicKeyString(), 10)
+    accountModel.updateBalance(wallet.publicKeyString(), -5)
 
-    blockchain = Blockchain()
+    pprint.pprint(accountModel.balances)
 
-    lastHash = BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-    block = wallet.createBlock(pool.transactions, lastHash, blockCount)
-
-    if not blockchain.lastBlockHashValid(block):
-        print('last block hash is not valid')
-
-    if not blockchain.blockCountValid(block):
-        print('last block count is not valid')
-
-    if blockchain.lastBlockHashValid(block) and blockchain.blockCountValid(block):
-        blockchain.addBlock(block)
-
-    pprint.pprint(blockchain.toJson())
+    
     
