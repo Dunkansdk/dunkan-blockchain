d45c23780e4229ea316a96e25484ed2abb335497
Dunkansdk <emanueldurso1@gmail.com>
2021-10-11 18:54:24 -0300

    P2P Socket communication module implemented

--- src/Main.py
+++ src/Main.py
@@ -1,52 +1,15 @@
-
 import pprint
-from BlockchainUtils import BlockchainUtils
+import sys
 
-from Wallet import Wallet
-from TransactionPool import TransactionPool
-from Block import Block
-from Blockchain import Blockchain
-from AccountModel import AccountModel
+from Node import Node
 
 if __name__ == '__main__':
-    
-    blockchain = Blockchain()
-    pool = TransactionPool()
-    
-    dunkanWallet = Wallet()
-    boneWallet = Wallet()
-    exchangeWallet = Wallet()
-    forgerWallet = Wallet()
 
-    # necessary for the p2p nodes architecture
-    exchangeTransaction = exchangeWallet.createTransaction(dunkanWallet.publicKeyString(), 10, 'EXCHANGE')
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
 
-    if not pool.transactionExists(exchangeTransaction):
-        pool.addTransaction(exchangeTransaction)
-
-    coveredTransaction = blockchain.getCoveredTransactionSet(pool.transactions)
-    lastHash = BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-    blockOne = forgerWallet.createBlock(coveredTransaction, lastHash, blockCount)
-    blockchain.addBlock(blockOne)
-
-    pool.remove_from_pool(blockOne.transactions)
-
-    # dunkan wants to send 5 token to bone
-    transaction = dunkanWallet.createTransaction(boneWallet.publicKeyString(), 5, 'TRANSFER')
-
-    if not pool.transactionExists(transaction):
-        pool.addTransaction(transaction)
-
-    coveredTransaction = blockchain.getCoveredTransactionSet(pool.transactions)
-    lastHash = BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-    blockTwo = forgerWallet.createBlock(coveredTransaction, lastHash, blockCount)
-    blockchain.addBlock(blockTwo)
-
-    pool.remove_from_pool(blockTwo.transactions)
-
-    pprint.pprint(blockchain.toJson())
+    node = Node(ip, port)
+    node.start_p2p()
 
     
     
